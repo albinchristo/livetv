a2e709757a7495e08b33761193b012a58a51c416
indiasports <60233516+indiasports@users.noreply.github.com>
2024-08-14 23:27:07 +0530

    Update

---
+++ script.js
@@ -0,0 +1,13 @@
+fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    console.log(data);
+    displayCocktail(data)
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
