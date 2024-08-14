4dc3f16f6944606ad280fee56a3b22d8f38acb57
indiasports <60233516+indiasports@users.noreply.github.com>
2024-08-14 23:28:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,37 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     displayCocktail(data)
   })
   .catch((error) => console.error("FETCH ERROR:", error));
+
+function displayCocktail(data) {
+  const cocktail = data.drinks[0];
+  const cocktailDiv = document.getElementById("cocktail");
+  // cocktail name
+  const cocktailName = cocktail.strDrink;
+  const heading = document.createElement("h1");
+  heading.innerHTML = cocktailName;
+  cocktailDiv.appendChild(heading);
+  // cocktail image
+  const cocktailImg = document.createElement("img");
+  cocktailImg.src = cocktail.strDrinkThumb;
+  cocktailDiv.appendChild(cocktailImg);
+  document.body.style.backgroundImage = "url('" + cocktail.strDrinkThumb + "')";
+  // cocktail ingredients
+  const cocktailIngredients = document.createElement("ul");
+  cocktailDiv.appendChild(cocktailIngredients);
+  const getIngredients = Object.keys(cocktail)
+    .filter(function (ingredient) {
+      return ingredient.indexOf("strIngredient") == 0;
+    })
+    .reduce(function (ingredients, ingredient) {
+      if (cocktail[ingredient] != null) {
+        ingredients[ingredient] = cocktail[ingredient];
+      }
+      return ingredients;
+    }, {});
+  for (let key in getIngredients) {
+    let value = getIngredients[key];
+    listItem = document.createElement("li");
+    listItem.innerHTML = value;
+    cocktailIngredients.appendChild(listItem);
+  }
+}
